89fbdb829e996fd4df5e1ed80212d44936dbd4ef
uharshita <77626860+uharshita@users.noreply.github.com>
2021-01-25 21:17:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function me() {
+  window.alert("This is me");
+}
+function dad() {
+  window.alert("This is my dad");
+}
+function mom() {
+  window.alert("This is my mother");
+}
+function mygrandma() {
+  window.alert("This is my Grandmother");
+}
+function my_with_my_dad_and_my_sis() {
+  window.alert("This is me with my dad and sister");
+}
+function mom_and_my_sis() {
+  window.alert("This is  my mom with sister");
+}
+function sister(){
+  window.alert("This is my sister");
+}
